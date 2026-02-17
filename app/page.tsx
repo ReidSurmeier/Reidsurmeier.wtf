ba555706b272df8aa8404d348045c05e23a8ed96
zhangsoyan <rsurmeie@risd.edu>
2026-02-16 21:15:31 -0500

    Add ASCII art banner to top of page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,35 @@
+/*
+      RSRSRSRSba   RSRSRSRSRS8  RS  RSRSRSRSba,
+      RS      "8b  RS           RS  RS      `"8b
+      RS      ,8P  RS           RS  RS        `8b
+      RSaaaaaa8P'  RSaaaaa      RS  RS         RS
+      RS""""RS'    RS"""""      RS  RS         RS
+      RS    `8b    RS           RS  RS         8P
+      RS     `8b   RS           RS  RS      .a8P
+      RS      `8b  RSRSRSRSRS8  RS  RSRSRSRSY"'
+
+
+       adRSRS8ba   RS        RS  RSRSRSRSba   RSb           dRS  RSRSRSRSRS8  RS  RSRSRSRSRS8  RSRSRSRSba
+      d8"     "8b  RS        RS  RS      "8b  RS8b         dRS8  RS           RS  RS           RS      "8b
+      Y8,          RS        RS  RS      ,8P  RS`8b       d8'RS  RS           RS  RS           RS      ,8P
+      `Y8aaaaa,    RS        RS  RSaaaaaa8P'  RS `8b     d8' RS  RSaaaaa      RS  RSaaaaa      RSaaaaaa8P'
+        `"""""8b,  RS        RS  RS""""RS'    RS  `8b   d8'  RS  RS"""""      RS  RS"""""      RS""""RS'
+              `8b  RS        RS  RS    `8b    RS   `8b d8'   RS  RS           RS  RS           RS    `8b
+      Y8a     a8P  Y8a.    .a8P  RS     `8b   RS    `RS8'    RS  RS           RS  RS           RS     `8b
+       "YRSRS8P"    `"YRSRSY"'   RS      `8b  RS     `8'     RS  RSRSRSRSRS8  RS  RSRSRSRSRS8  RS      `8b
+
+
+       I8,        8        ,8I  RSRSRSRSRSRS  RSRSRSRSRS8
+       `8b       d8b       d8'       RS       RS
+        "8,     ,8"8,     ,8"        RS       RS
+         Y8     8P Y8     8P         RS       RSaaaaa
+         `8b   d8' `8b   d8'         RS       RS"""""
+          `8a a8'   `8a a8'          RS       RS
+      RS8  `8a8'     `8a8'           RS       RS
+      RS8   `8'       `8'            RS       RS
+
+      (graphic design and programming by Reid Surmeier)
+*/
 "use client";
 
 import { useState, useCallback, useEffect, useRef, forwardRef } from "react";
